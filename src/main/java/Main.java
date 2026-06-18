ecf1ec475863b466e770650b27f14626ebf4b849
Hardik Hansraj <hardik@Hardiks-MacBook-Air.local>
2026-06-18 11:43:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,14 +7,27 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+            
+            if (input == null || input.trim().isEmpty()) continue;
 
             String[] tokens = input.trim().split("\\s+");
             String command = tokens[0];
             
             if (command.equals("exit")) {
                 break;
+                
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+                
+            } else if (command.equals("type")) {
+                String target = tokens[1];
+                
+                if (target.equals("echo") || target.equals("exit") || target.equals("type")) {
+                    System.out.println(target + " is a shell builtin");
+                } else {
+                    System.out.println(target + ": not found");
+                }
+                
             } else {
                 System.out.println(command + ": command not found");
             }
