d62209bfb17a096f6bdaa04f2fa69a19c7898918
Hardik Hansraj <hardik@Hardiks-MacBook-Air.local>
2026-06-18 16:02:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -175,4 +175,111 @@ public class Main {
                 if (appendErr) {
                     Files.writeString(Paths.get(errFile), "", StandardOpenOption.CREATE, StandardOpenOption.APPEND);
                 } else {
-                    Files.writeString(Paths.get(errFile), "", StandardOpenOption.
+                    Files.writeString(Paths.get(errFile), "", StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                }
+            }
+            
+            if (command.equals("exit")) {
+                break;
+                
+            } else if (command.equals("jobs")) {
+                for (Job job : backgroundJobs) {
+                    if (job.process.isAlive()) {
+                        String output = String.format("[%d]+  Running                 %s", job.id, job.command);
+                        printOut(output, outFile, appendOut);
+                    }
+                }
+                
+            } else if (command.equals("echo")) {
+                StringBuilder echoOutput = new StringBuilder();
+                for (int i = 1; i < tokens.length; i++) {
+                    echoOutput.append(tokens[i]);
+                    if (i < tokens.length - 1) {
+                        echoOutput.append(" ");
+                    }
+                }
+                printOut(echoOutput.toString(), outFile, appendOut);
+                
+            } else if (command.equals("pwd")) {
+                printOut(currentDir.toString(), outFile, appendOut);
+                
+            } else if (command.equals("cd")) {
+                String target = tokens.length > 1 ? tokens[1] : "";
+                
+                if (target.equals("~")) {
+                    currentDir = Paths.get(System.getenv("HOME"));
+                } else {
+                    Path newPath = currentDir.resolve(target).normalize();
+                    if (new File(newPath.toString()).isDirectory()) {
+                        currentDir = newPath;
+                    } else {
+                        System.out.println("cd: " + target + ": No such file or directory");
+                    }
+                }
+                
+            } else if (command.equals("type")) {
+                String target = tokens.length > 1 ? tokens[1] : "";
+                if (target.equals("echo") || target.equals("exit") || target.equals("type") || target.equals("pwd") || target.equals("cd") || target.equals("jobs")) {
+                    printOut(target + " is a shell builtin", outFile, appendOut);
+                } else {
+                    String pathEnv = System.getenv("PATH");
+                    boolean found = false;
+                    if (pathEnv != null) {
+                        String[] directories = pathEnv.split(":"); 
+                        for (String dir : directories) {
+                            File file = new File(dir, target);
+                            if (file.exists() && file.canExecute()) {
+                                printOut(target + " is " + file.getAbsolutePath(), outFile, appendOut);
+                                found = true;
+                                break; 
+                            }
+                        }
+                    }
+                    if (!found) {
+                        printOut(target + ": not found", outFile, appendOut);
+                    }
+                }
+                
+            } else {
+                try {
+                    ProcessBuilder pb = new ProcessBuilder(tokens);
+                    pb.directory(new File(currentDir.toString())); 
+                    
+                    pb.inheritIO();
+                    
+                    if (outFile != null) {
+                        File out = new File(outFile);
+                        out.getParentFile().mkdirs();
+                        if (appendOut) {
+                            pb.redirectOutput(ProcessBuilder.Redirect.appendTo(out));
+                        } else {
+                            pb.redirectOutput(out);
+                        }
+                    }
+                    
+                    if (errFile != null) {
+                        File err = new File(errFile);
+                        err.getParentFile().mkdirs();
+                        if (appendErr) {
+                            pb.redirectError(ProcessBuilder.Redirect.appendTo(err));
+                        } else {
+                            pb.redirectError(err);
+                        }
+                    }
+                    
+                    Process process = pb.start();
+                    
+                    if (isBackground) {
+                        System.out.println("[" + nextJobId + "] " + process.pid());
+                        backgroundJobs.add(new Job(nextJobId, process, fullCommand));
+                        nextJobId++;
+                    } else {
+                        process.waitFor();
+                    }
+                } catch (Exception e) {
+                    System.out.println(command + ": command not found");
+                }
+            }
+        }
+    }
+}
